162e9eba869b2d663dfcaad8980dfd0b88393d11
saurabh15may <saurabh15may@gmail.com>
2017-08-26 01:06:31 +1000

    Added REST client code to consume Weather API from Openweathermap

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -3,6 +3,10 @@ package com.serverless;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.apache.log4j.Logger;
 
@@ -16,7 +20,17 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: " + input);
-		Response responseBody = new Response("Hello, the current time is " + new Date());
+		String rest_api_response = null;
+		String restURL = "http://samples.openweathermap.org/data/2.5/weather?id=7839805&appid=b1b15e88fa797225412429c1c50c122a1";
+		
+		try{
+			rest_api_response = callRestAPI(restURL);
+		} catch (Exception e) {
+			System.out.print("Error while calling REST API" + e);
+		}
+		
+		Response responseBody = new Response("The weather details of Melbourne from OpenWeather API call " + rest_api_response);
+
 		Map<String, String> headers = new HashMap<>();
 		headers.put("X-Powered-By", "AWS Lambda & Serverless");
 		headers.put("Content-Type", "application/json");
@@ -26,4 +40,22 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 				.setHeaders(headers)
 				.build();
 	}
+
+	private static String callRestAPI(String restURL) throws Exception {
+        URL obj = new URL(restURL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = con.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = reader.readLine()) != null) {
+            response.append(inputLine);
+        }
+        reader.close();
+
+        return response+"";
+    }
 }
