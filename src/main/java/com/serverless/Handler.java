7ac452b9ff8c6415798bacc4d5d682ca9ae1c68f
saurabh15may <saurabh15may@gmail.com>
2017-09-01 02:26:00 +1000

    Externalize API base URL to serverless environment variable

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -23,42 +23,23 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		String cityId = (String) input.get("id");
-		LOG.info("cityId: " + cityId);
 		String rest_api_response = null;
-		String restURL = "http://api.openweathermap.org/data/2.5/weather?id="+ cityId +"&APPID=0c87cd4372ac1f3b58570b17e8f7326b&units=metric";
-		LOG.info("restURL: " + restURL);
+
+		//Getting the value from environment variable defined in serverless.yml
+		String WEATHER_API_BASE_URL = System.getenv("WEATHER_API_BASE_URL");
+
+		String restURL = WEATHER_API_BASE_URL + "&id="+ (String)input.get("id");
+		LOG.info("restURL: "+restURL);
 
 		try{
 			rest_api_response = callRestAPI(restURL);
-
 		} catch (Exception e) {
 			LOG.error("Error while calling REST API" + e);
 		}
 		
-		Response responseBody = new Response();
-
-		JsonParser parser=new JsonParser();
-		JsonObject object=(JsonObject)parser.parse(rest_api_response);
-
-		String weather = object.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("description").getAsString();
-		String temperature = object.get("main").getAsJsonObject().get("temp").getAsString();
-		String city = object.get("name").getAsString();
-		String wind = object.get("wind").getAsJsonObject().get("speed").getAsString();
-		wind = wind + " m/s";
-		String pattern = "EEEE h:mm a";
-		SimpleDateFormat format = new SimpleDateFormat(pattern);
-		String time = format.format(new Date());
-
-		responseBody.setCity(city);
-		responseBody.setTemperature(temperature);
-		responseBody.setUpdatedTime(time);
-		responseBody.setWeather(weather);
-		responseBody.setWind(wind);
-
-		Map<String, String> headers = new HashMap<>();
-		headers.put("X-Powered-By", "AWS Lambda & Serverless");
-		headers.put("Content-Type", "application/json");
+		Response responseBody = createResponseObject(rest_api_response);
+		Map<String, String> headers = createHeader();
+		
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
@@ -66,21 +47,48 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 				.build();
 	}
 
-	private static String callRestAPI(String restURL) throws Exception {
-        URL obj = new URL(restURL);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        int responseCode = con.getResponseCode();
-        System.out.println("Response Code : " + responseCode);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = reader.readLine()) != null) {
-            response.append(inputLine);
-        }
-        reader.close();
+	private static Map<String, String> createHeader(){
+		Map<String, String> headers = new HashMap<>();
+		headers.put("X-Powered-By", "AWS Lambda & Serverless");
+		headers.put("Content-Type", "application/json");
+		return headers;
+	}
+	private static Response createResponseObject(String  rest_api_response){
+		Response responseBody = new Response();
+		
+		JsonParser parser=new JsonParser();
+		JsonObject object=(JsonObject)parser.parse(rest_api_response);
 
-        return response+"";
+		String weather = object.get("weather").getAsJsonArray().get(0).getAsJsonObject().get("description").getAsString();
+		String temperature = object.get("main").getAsJsonObject().get("temp").getAsString();
+		String city = object.get("name").getAsString();
+		String wind = object.get("wind").getAsJsonObject().get("speed").getAsString() + " m/s";
+		String time = new SimpleDateFormat("EEEE h:mm a").format(new Date());
+
+		responseBody.setCity(city);
+		responseBody.setTemperature(temperature);
+		responseBody.setUpdatedTime(time);
+		responseBody.setWeather(weather);
+		responseBody.setWind(wind);
+
+		return responseBody;
+	}
+
+	private static String callRestAPI(String restURL) throws Exception {
+		URL obj = new URL(restURL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		int responseCode = con.getResponseCode();
+		System.out.println("Response Code : " + responseCode);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuilder response = new StringBuilder();
+		while ((inputLine = reader.readLine()) != null) {
+			response.append(inputLine);
+		}
+		reader.close();
+
+		return response+"";
     }
 }
